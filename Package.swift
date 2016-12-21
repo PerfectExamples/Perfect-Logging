f29752d81d6133f71b2db7bb47b90e6622237e26
Jonathan Guthrie <jono@perfect.org>
2016-12-21 13:45:22 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2016-12-21 18:22:50 +0000
+import PackageDescription
+let package = Package(
+    name: "Perfect-Logging",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
+    ]
+)
---
+++ Sources/main.swift
@@ -0,0 +1,66 @@
+//
+//  main.swift
+//  PerfectTemplate
+//
+//  Created by Kyle Jessup on 2015-11-05.
+//	Copyright (C) 2015 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+import PerfectLogger
+
+// Define custom log file
+LogFile.location = "./customLogFile.log"
+
+
+// Get an event id so all messages at this level can be "linked" for analysis
+let eventid = LogFile.info("Server Setup message")
+
+
+// Create HTTP server.
+let server = HTTPServer()
+
+// Register your own routes and handlers
+var routes = Routes()
+routes.add(method: .get, uri: "/", handler: {
+		request, response in
+		response.setHeader(.contentType, value: "text/html")
+
+		LogFile.debug("This is a request log event fired when route hit by a browser.")
+
+		response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+		response.completed()
+	}
+)
+
+// Add the routes to the server.
+server.addRoutes(routes)
+
+// Set a listen port of 8181
+server.serverPort = 8181
+
+LogFile.info("Server port is: \(server.serverPort)", eventid: eventid)
+
+LogFile.debug("Demonstrating a logging event without the linked event id")
+
+
+do {
+	// Launch the HTTP server.
+	LogFile.info("Starting server", eventid: eventid)
+	try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+	print("Network error thrown: \(err) \(msg)")
+}

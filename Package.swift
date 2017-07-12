b314f1c7ec721a2a8ef6936f7639fabafcb4ae9e
Jonathan Guthrie <jono@perfect.org>
2017-07-12 11:37:09 -0400

    update versions

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-21 18:22:50 +0000
+// Date: 2017-07-12 15:31:36 +0000
 import PackageDescription
 let package = Package(
-    name: "Perfect-Logging",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
-    ]
+	name: "Perfect-Logging",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
+	]
 )
